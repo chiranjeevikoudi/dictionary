4dc8b9d5116b347d3e87ea850a42536a9c5a3f91
chiru <chirukkoudi@gmail.com>
2019-01-27 01:46:53 +0530

    antonyms function added

--- api/api-utils.js
+++ api/api-utils.js
@@ -107,11 +107,54 @@ function getSynonyms(word, callback) {
     });
 }
 
+function getAntonyms(word, callback) {
+    let url = apiUrls.URLS.BASE_URL+"/"+word+"/"+ apiUrls.URLS.ANTONYMS+"&api_key="+apiUrls.API_KEY;
+    getWordDataFrom(url, function (err, antonyms) {
+        if (err) {
+            callback(err);
+        }
+        else if (antonyms) {
+            if(Object.prototype.toString.call(antonyms).indexOf("Array")>-1){
+                if(antonyms.length !== 0){
+                    let antonymsArray = [];
+                    for(let index=0; index<antonyms.length; index++){
+                        if(antonyms[index]["relationshipType"] && antonyms[index]["relationshipType"] === "antonym"){
+                            if(antonyms[index]["words"]){
+                                antonymsArray = antonyms[index]["words"];
+                            }
+                            break;
+                        }
+                    }
+                    if(antonymsArray.length !== 0){
+                        callback(null,antonymsArray);
+                    }
+                    else{
+                        callback(null,null);
+                    }
+                }
+                else{
+                    callback(null,null);
+                }
+            }
+            else if(isApiKeyValid(antonyms)){
+                callback(errorCodes.INVALID_API_KEY);
+            }
+            else{
+                callback(errorCodes.INTERNAL_ERROR);
+            }
+        }
+        else{
+            callback(null,null);
+        }
+    });
+}
+
+
 
 
 apiUtils.getDefinitions = getDefinitions;
-
 apiUtils.getSynonyms = getSynonyms;
+apiUtils.getAntonyms = getAntonyms;
 
 
 module.exports = apiUtils;
